8f32a538c19a9aa995358e2860084bb5ac3244a3
vienson <jeetkun@gmail.com>
2024-08-08 15:44:12 -0400

    add auth message" ;

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 
 
 const start = async () => {
+   console.log('auth start');
    if(process.env.JWT_KEY===undefined) throw new Error('JWT_KEY must be defined');
    if(process.env.MONGO_URI===undefined) throw new Error('MONGO_URI must be defined');
    try {
